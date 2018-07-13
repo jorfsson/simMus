a62dbeda9a908a22fe05a68b27e7627bf33ceda2
Jeffrey Chea <jeffreychea234@gmail.com>
2018-07-13 11:26:37 -0500

    work on heroku functions

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParse = require('body-parser')
 const PORT = process.env.PORT;
 const app = express()
 
+app.set('port', PORT || 3000);
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
 

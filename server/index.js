395328496d4e978abed5d45cb0aee812febeb7c2
Jeffrey Chea <jeffreychea234@gmail.com>
2018-07-13 11:23:37 -0500

    work on completing heroku functionality

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParse = require('body-parser')
+const PORT = process.env.PORT;
 const app = express()
 
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log('listening on port 3000!');
 });

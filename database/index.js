47434deae8ee6d199a27b58da1e1e48c7597e0a8
Jeffrey Chea <jeffreychea234@gmail.com>
2018-07-14 18:13:36 -0500

    refactor to add to similar table

--- database/index.js
+++ database/index.js
@@ -57,9 +57,19 @@ var update = function(callback, table, data) {
   });
 };
 
-
+var addSimilar = function(callback, data) {
+  connection.query(`INSERT INTO similar (artist_id, similar_id) VALUES ((SELECT id FROM artist WHERE name=?), (SELECT id FROM artist WHERE name=?))`, data, function(err, results, fields) {
+    if  (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+// insert into similar (artist_id, similar_id) VALUES ((SELECT id from artist where name='Justin Vernon'), (select id from artist where name='Fleet Foxes'));
 
 module.exports.selectAll = selectAll;
 module.exports.insert = insert;
 module.exports.findDuplicateArtist = findDuplicateArtist;
 module.exports.update = update;
+module.exports.addSimilar = addSimilar;
--- server/index.js
+++ server/index.js
@@ -15,31 +15,34 @@ app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
 
 app.use('/search', function(req, res){
-  request( {
+  request({
     url: 'http://ws.audioscrobbler.com/2.0/',
     type: 'GET',
     qs: {
-    method: 'artist.getsimilar',
-    artist: req.query.body,
-    api_key: '04c96ec32bbace5646ad77d7c171ae4a' ,
-    format: 'json'
-  },
+      method: 'artist.getsimilar',
+      artist: req.query.body,
+      api_key: '04c96ec32bbace5646ad77d7c171ae4a' ,
+      format: 'json'
+    },
     headers: {
       "Content-Type": "application/json"
     }
- }, function(err, response, body){
-   if (err) {
-     throw err
-   }
-   res.set('Content-Type', 'application/json');
-   var parsedBody = JSON.parse(body);
-   // console.log(parsedBody.similarartists.artist[0])
+  }, function(err, response, body){
+    if (err) {
+      throw err
+    }
+    res.set('Content-Type', 'application/json');
+    var parsedBody = JSON.parse(body);
+    db.insert((err, data)=>{
+      if (err) console.log(err)
+    }, 'artist', {name: req.query.body});
    parsedBody.similarartists.artist.forEach(function(artist){
-     db.insert((err, data)=>{
-       if (err) {
-         console.log(err);
-       };
+     db.insert((err, data) => {
+       if (err) console.log(err);
      }, 'artist', {name: artist.name})
+     db.addSimilar((err, data) => {
+       if (err) console.log(err);
+     }, [req.query.body, artist.name])
    })
    res.end(body);
  })

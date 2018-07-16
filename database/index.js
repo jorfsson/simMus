da546c20598a5a6d3be651451ff49c4db2408e41
Jeffrey Chea <jeffreychea234@gmail.com>
2018-07-16 14:38:18 -0500

    change to postgres

--- database/index.js
+++ database/index.js
@@ -1,9 +1,9 @@
 var pg = require('pg');
 
-var newsql = new pg.Client
-var connection = newsql.connect(
+var connection = new pg.Client(
 'postgres://auveunhkxoqjlx:d0dc24109a6d93a5fd01fd60bf54467425b7277576056f860ff0ef2a4ef1b4bb@ec2-54-227-240-7.compute-1.amazonaws.com:5432/d2tbdogc0lfloc'
 );
+
 
 connection.connect(function(err) {
   if (err) { throw err } else { console.log('connected') }

4d77f8f96d4ace7c36f9104e6189cafe4f81c4a6
Jeffrey Chea <jeffreychea234@gmail.com>
2018-07-16 14:31:54 -0500

    change to postgres

--- database/index.js
+++ database/index.js
@@ -1,73 +1,69 @@
-// var mysql = require('mysql');
-//
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '!Srekwah1',
-//   socketPath: '/var/run/mysqld/mysqld.sock',
-//   database: 'artists'
-// });
-//
-// connection.connect(function(err) {
-//   if (err) { throw err } else { console.log('connected') }
-// });
-//
-// const selectAll = (table) =>
-//   new Promise((resolve, reject) => {
-//     connection.query(`SELECT * FROM ${table}`, (err, results) => {
-//       if (err) { reject(err) } else { resolve(results) }
-//     });
-//   });
-//
-//
-// const findDuplicateArtist = (table, artist) =>
-//   new Promise((resolve, reject) => {
-//     connection.query(`SELECT * FROM ${table} WHERE name = ?`, [artist], (err, results) => {
-//       if (err) { reject(err) } else { resolve(results) }
-//     });
-//   })
-//
-// const insert = (table, data) =>
-//   new Promise ((resolve, reject) => {
-//     connection.query(`INSERT INTO ${table} SET ?`, data, (err, results) => {
-//       if (err) { reject(err) } else { resolve(results) }
-//     });
-//   });
-//
-// const update = (table, data) =>
-//   new Promise ((resolve, reject) => {
-//     connection.query(`UPDATE ${table} SET ? WHERE name = ?`, data, (err, results) => {
-//       if (err) { reject(err) } else { resolve(results) }
-//     });
-//   })
-//
-// const addSimilar = (data) =>
-//   new Promise ((resolve, reject) => {
-//     connection.query(`INSERT INTO similar (artist_id, similar_id) VALUES ((SELECT id FROM artist WHERE name=?), (SELECT id FROM artist WHERE name=?))`, data, (err, results) => {
-//       if (err) { reject(err) } else { resolve(results) }
-//     });
-//   });
-//
-// const checkID = (data) =>
-//   new Promise ((resolve, reject) => {
-//     connection.query(`SELECT similar. FROM similar INNER JOIN artist ON ?))`, data, (err, results) => {
-//       if (err) { reject(err) } else { resolve(results) }
-//     });
-//   });
-//
-//
-// const addArtist = (artistName) =>
-//   findDuplicateArtist('artist', artistName)
-//   .then((data) => {
-//     if (data.length === 0) {
-//       insert('artist', {name: artistName}).then((data) => {console.log(data)})
-//     } else { console.log('Already exists in database')}
-//   }).catch((err) => {console.log(err)})
-//
-//
-// module.exports.selectAll = selectAll;
-// module.exports.insert = insert;
-// module.exports.findDuplicateArtist = findDuplicateArtist;
-// module.exports.update = update;
-// module.exports.addSimilar = addSimilar;
-// module.exports.addArtist = addArtist;
+var mysql = require('pg');
+
+var connection = mysql.createConnection(
+'postgres://auveunhkxoqjlx:d0dc24109a6d93a5fd01fd60bf54467425b7277576056f860ff0ef2a4ef1b4bb@ec2-54-227-240-7.compute-1.amazonaws.com:5432/d2tbdogc0lfloc'
+);
+
+connection.connect(function(err) {
+  if (err) { throw err } else { console.log('connected') }
+});
+
+const selectAll = (table) =>
+  new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM ${table}`, (err, results) => {
+      if (err) { reject(err) } else { resolve(results) }
+    });
+  });
+
+
+const findDuplicateArtist = (table, artist) =>
+  new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM ${table} WHERE name = ?`, [artist], (err, results) => {
+      if (err) { reject(err) } else { resolve(results) }
+    });
+  })
+
+const insert = (table, data) =>
+  new Promise ((resolve, reject) => {
+    connection.query(`INSERT INTO ${table} SET ?`, data, (err, results) => {
+      if (err) { reject(err) } else { resolve(results) }
+    });
+  });
+
+const update = (table, data) =>
+  new Promise ((resolve, reject) => {
+    connection.query(`UPDATE ${table} SET ? WHERE name = ?`, data, (err, results) => {
+      if (err) { reject(err) } else { resolve(results) }
+    });
+  })
+
+const addSimilar = (data) =>
+  new Promise ((resolve, reject) => {
+    connection.query(`INSERT INTO similar (artist_id, similar_id) VALUES ((SELECT id FROM artist WHERE name=?), (SELECT id FROM artist WHERE name=?))`, data, (err, results) => {
+      if (err) { reject(err) } else { resolve(results) }
+    });
+  });
+
+const checkID = (data) =>
+  new Promise ((resolve, reject) => {
+    connection.query(`SELECT similar. FROM similar INNER JOIN artist ON ?))`, data, (err, results) => {
+      if (err) { reject(err) } else { resolve(results) }
+    });
+  });
+
+
+const addArtist = (artistName) =>
+  findDuplicateArtist('artist', artistName)
+  .then((data) => {
+    if (data.length === 0) {
+      insert('artist', {name: artistName}).then((data) => {console.log(data)})
+    } else { console.log('Already exists in database')}
+  }).catch((err) => {console.log(err)})
+
+
+module.exports.selectAll = selectAll;
+module.exports.insert = insert;
+module.exports.findDuplicateArtist = findDuplicateArtist;
+module.exports.update = update;
+module.exports.addSimilar = addSimilar;
+module.exports.addArtist = addArtist;
